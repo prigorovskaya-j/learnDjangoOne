8d5d61d9ca714d59ed991f06211caaa0a044f08f
prigorovskaya-j <prigorovskaya.j.i.2.18@gmail.com>
2022-01-29 18:29:56 +0300

    #4. Определение моделей. Миграции: создание и выполнение

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4mn*^85=b9hd0zf)ewol1ou)=ctcc$9p2he5!hnsrumj)_0s@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+#DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1']
-
 
 # Application definition
 
@@ -122,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
