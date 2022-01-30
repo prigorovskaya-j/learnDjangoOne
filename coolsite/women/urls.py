4cf386b12ff239e74509f212e0119039e9712841
prigorovskaya-j <prigorovskaya.j.i.2.18@gmail.com>
2022-01-30 15:34:19 +0300

    #6. Шаблоны (templates). Начало

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,7 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<int:catId>/', categories),  # http://127.0.8.1:8000/women/cats/1/
-    path('main/', main),
-    re_path(r'^archive/(?P<year>[0-9]{4})', archive),
+    path('about/', about, name='about')
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
 
 # Create your views here.
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
 def index(request):
-    return HttpResponse("Страница приложения women")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts':posts, 'menu':menu,'title':'Главная страница'})
+
+def about(request):
+    return render(request, 'women/about.html', {'menu':menu, 'title':'О сайте'})
 
 def categories(request, catId):
     if request.GET:
