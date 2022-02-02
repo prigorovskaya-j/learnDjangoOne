dd2e0bc7308c27727216978dd155f1826cce790f
prigorovskaya-j <prigorovskaya.j.i.2.18@gmail.com>
2022-02-02 21:35:20 +0300

    #9. Создание связей между моделями через класс ForeignKey

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -13,10 +13,14 @@ menu = [
 ]
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -34,6 +38,22 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id={post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts)==0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Упс. Страница не найдена</h1>')
