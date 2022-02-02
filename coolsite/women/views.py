172c6d162fb7c4db7c0c4e865f2addb3d6de783b
prigorovskaya-j <prigorovskaya.j.i.2.18@gmail.com>
2022-02-02 19:18:50 +0300

    #8. Формирование URL-адресов в шаблонах

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -5,31 +5,35 @@ from .models import *
 
 # Create your views here.
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
-
+menu = [
+    {'title': "О сайте", 'url_name':'about'},
+    {'title':"Добавить статью", 'url_name':'add_page'},
+    {'title':"Обратная связь", 'url_name':'contact'},
+    {'title':"Войти", 'url_name':'login'}
+]
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts':posts, 'menu':menu,'title':'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu':menu, 'title':'О сайте'})
 
-def categories(request, catId):
-    if request.GET:
-        print(request.GET)
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>Страница {catId}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
-def main(request):
-    return HttpResponse("<h1>Hello, Python</h1>")
+def contact(request):
+    return HttpResponse("Обратная связь")
 
-def archive(request, year):
-    if int(year)>2022:
-        raise Http404()
-    if int(year)<1999:
-        return redirect('home', permanent=True)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id={post_id}")
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Упс. Страница не найдена</h1>')
